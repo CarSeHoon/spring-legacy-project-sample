0de2509bf0f0535748e5c322c5c386e52427c240
CarSeHoon <sgd222@naver.com>
2022-10-31 11:42:42 +0900

    221031 차세훈 test.java 수정

--- spring-lecacy-project/back-end/main/java/kr/co/edu/test/Test.java
+++ spring-lecacy-project/back-end/main/java/kr/co/edu/test/Test.java
@@ -6,5 +6,6 @@ public class Test {
 		TestClass testClass = new TestClass();	//new -> TestClass를 지속적으로 사용하기 위해
 		int result = testClass.sum(1, 1);
 		System.out.print("result : " + result);
+		System.out.println("hello test");
 	}
 }
